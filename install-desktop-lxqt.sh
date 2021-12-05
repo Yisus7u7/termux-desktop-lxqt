bb8e4b546b0c69b33c5185a77d6dd51693a939ee
Learn-Termux <91124025+Learn-Termux@users.noreply.github.com>
2021-12-05 10:39:56 +0530

    Update install-desktop-lxqt.sh

--- install-desktop-lxqt.sh
+++ install-desktop-lxqt.sh
@@ -12,7 +12,8 @@ clear
 python termux-desktop-lxqt/lxqt.py
 mv termux-desktop/main.py $PREFIX/bin/e-tnv
 #pkg in wget;wget https://github.com/Learn-Termux/env-distro/raw/main/e-tnv.sh;chmod +x e-tnv.sh;./e-tnv.sh
-
+wget https://github.com/Learn-Termux/env-distro/raw/main/font.ttf -O .termux/font.ttf
+termux-reload-settings
 echo -e """\e[1;32menjoy!!
 To Start The Vnc Server, Type e-tnv start
 To Stop It, Type e-tnv stopall
